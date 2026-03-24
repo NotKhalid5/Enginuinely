038bf185165da95b093768be5e4c055bec09539a
NotKhalid5 <khalidajr@icloud.com>
2026-03-24 00:27:18 -0400

    Attempted to fix movement in simulation. Objects don't move. Debugging now. (Plan to implement more accurate integration method in attempts).

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,14 @@ public class Main {
         // 1st body
         Body body1 = new Body();
         body1.position = new Vector2(0,0);
-        body1.velocity = new Vector2(2,0);
+        body1.velocity = new Vector2(200,0);
         body1.mass = 1.0;
         body1.shape = new Circle(20.0);
 
         // 2nd body
         Body body2 = new Body();
         body2.position = new Vector2(50,0);
-        body2.velocity = new Vector2(-2,0);
+        body2.velocity = new Vector2(-200,0);
         body2.mass = 1.0;
         body2.shape = new Circle(20.0);
 
@@ -38,7 +38,7 @@ public class Main {
         // simulation loop
         while(true) {
             double now = System.nanoTime();
-            double dt = (now -lastTime) / 1e9; // time step n secs
+            double dt = (now - lastTime) / 1e9; // time step n secs
             lastTime = now;
 
             infinity.step(dt); // step sim forward
@@ -46,7 +46,7 @@ public class Main {
             // Debug output
             System.out.println("Body1: (" + body1.position.x + ", " + body1.position.y + ")");
             System.out.println("Body2: (" + body2.position.x + ", " + body2.position.y + ")");
-
+//            i just talked to Nadia, and we talked about Audre Lorde
             try {
                 Thread.sleep(16);
             } catch (InterruptedException e) {
